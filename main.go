6661658d97e34f2e0b6a0735ca7817d7c6cc9ca9
Raphael Ribeiro <raphaelkribeiro@gmail.com>
2024-10-07 13:14:55 -0400

    Sets up a basic server.

--- main.go
+++ main.go
@@ -1,36 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	corsMux := middlewareCors(mux)
 
-	const addr = ":8080"
-
-	srv := http.Server{
-		Handler: corsMux,
-		Addr:    addr,
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
 	}
-	// this blocks forever, until the server
-	// has an unrecoverable error
-	log.Println("server started on ", addr)
-	err := srv.ListenAndServe()
-	log.Fatal(err)
-}
 
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
+	fmt.Println("Starting server on port 8080")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalln(err)
+	}
 }
