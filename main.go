94cf9eaed0f8f32b5f9cba490ea955397c23c3c7
Raphael Ribeiro <raphaelkribeiro@gmail.com>
2024-10-07 13:31:03 -0400

    Uses the golan's http fileserver to server index html.

--- main.go
+++ main.go
@@ -6,11 +6,19 @@ import (
 	"net/http"
 )
 
+const (
+	port    = 8080
+	rootDir = "."
+)
+
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(rootDir)))
+
+	addr := fmt.Sprintf(":%d", port)
 
 	server := http.Server{
-		Addr:    ":8080",
+		Addr:    addr,
 		Handler: mux,
 	}
 
