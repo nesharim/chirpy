71e59ebe56976e55590260c7e2b2dddf30dfe400
Raphael Ribeiro <raphaelkribeiro@gmail.com>
2024-10-08 01:47:12 -0400

    Added middleware for to capture metrics of the route hit.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 const (
@@ -11,11 +12,25 @@ const (
 	rootDir = "."
 )
 
+type ApiConfig struct {
+	fileServerHits atomic.Int64
+}
+
+func (cfg *ApiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	apiCfg := ApiConfig{fileServerHits: atomic.Int64{}}
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(rootDir))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(rootDir)))))
 	mux.HandleFunc("/healthz", handleReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handleMetrics)
+	mux.HandleFunc("/reset", apiCfg.handleReset)
 
 	addr := fmt.Sprintf(":%d", port)
 
@@ -28,6 +43,15 @@ func main() {
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func (cfg *ApiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	text := fmt.Sprintf("Hits: %d", cfg.fileServerHits.Load())
+	w.Write([]byte(text))
+}
+
+func (cfg *ApiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileServerHits.Store(0)
 }
 
 func handleReadiness(w http.ResponseWriter, r *http.Request) {
