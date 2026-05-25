e2f4a17c7a62317d148f72106af5ed50f0037383
Nesharim <raphaelkribeiro@gmail.com>
2026-05-25 16:37:39 -0400

    Moved handlers to thier dedicated files.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -11,44 +10,21 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) getHitsHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	if _, err := fmt.Fprintf(w, "Hits: %v", cfg.fileserverHits.Load()); err != nil {
-		log.Fatalf("Failed to get total hits: %v", err)
-	}
-}
-
-func (cfg *apiConfig) resetAPIConfigHandler(w http.ResponseWriter, req *http.Request) {
-	cfg.fileserverHits.Store(0)
-}
-
-func healthCheckHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	if _, err := w.Write([]byte(http.StatusText(http.StatusOK))); err != nil {
-		log.Printf("Failed to write to body: %v", err)
-	}
-}
-
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
-	apicfg := &apiConfig{}
-	mux := http.NewServeMux()
+	apiConfig := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
-	mux.Handle("/app/", apicfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-	mux.HandleFunc("GET /healthz", healthCheckHandler)
-	mux.HandleFunc("GET /metrics", apicfg.getHitsHandler)
-	mux.HandleFunc("POST /reset", apicfg.resetAPIConfigHandler)
+	mux := http.NewServeMux()
+	fsHandler := apiConfig.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+
+	mux.Handle("/app/", fsHandler)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /api/metrics", apiConfig.handlerMetrics)
+	mux.HandleFunc("POST /api/reset", apiConfig.handlerReset)
 
 	server := &http.Server{
 		Addr:    ":" + port,
